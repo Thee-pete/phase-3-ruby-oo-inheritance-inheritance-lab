d7c9f8662556105452658dfc134252b7fcbcfe43
Peter Ndumia <ndegwandumia@gmail.com>
2022-08-26 12:03:22 +0300

    Complete

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+require_relative './user'
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
+
+    def learn (lesson)
+        @knowledge << lesson
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+
 
 end
